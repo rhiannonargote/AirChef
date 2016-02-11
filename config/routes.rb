8abe77e6bf155724fdfa6b687d4d1e5db109c21e
Rhiannon Argote <rhiannon.argote@gmail.com>
2016-02-11 16:55:53 +1100

    Added features in Reservations Controller and Views

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get '/preview' => 'reservations#preview'
 
   get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
 
 end
 
