c426f69a284f10cf0a55649d472c2d1de7d4f246
Rhiannon Argote <rhiannon.argote@gmail.com>
2016-02-11 18:16:27 +1100

    Message Views Added

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,26 +1,25 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
 
-before_action :authenticate_user
-
-def index
-  @users =User.all
-  @conversations = Conversation.involving(current_user)
-end
+  def index
+    @users = User.all
+    @conversations = Conversation.involving(current_user)
+  end
 
   def create
     if Conversation.between(params[:sender_id], params[:recipient_id]).present?
       @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
     else
-      @conversation = Conversation.create(conversations_params)
+      @conversation = Conversation.create(conversation_params)
+    end
+
+    redirect_to conversation_messages_path(@conversation)
   end
 
-  redirect_to conversation_messages_path(@conversation)
-end
+  private
 
-private  
-
-  def conversations_params
-    params.permit(:sender_id, :recipient_id)
-  end
+    def conversation_params
+      params.permit(:sender_id, :recipient_id)
+    end
 
 end
