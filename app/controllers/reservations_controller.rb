77260bd42dcd04b4983e6cc97be50ec7ad76de45
Rhiannon Argote <rhiannon.argote@gmail.com>
2016-02-12 11:21:38 +1100

    Final style changes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -29,12 +29,12 @@ class ReservationsController < ApplicationController
         business: 'rhiannon-facilitator@travlyfe.com',
         cmd: '_xclick',
         upload: 1,
-        notify_url: 'http://0d906f51.ngrok.io/notify',
+        notify_url: 'http://ea8511a5.ngrok.io/notify',
         amount: @reservation.total,
         item_name: @reservation.room.listing_name,
         item_number: @reservation.id,
         quantity: '1',
-        return: 'http://0d906f51.ngrok.io/your_trips'
+        return: 'http://ea8511a5.ngrok.io/your_trips'
       }
 
       redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
