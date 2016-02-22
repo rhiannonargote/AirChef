06cdf84d7d921519fc2b65dbedb4d2dd01fb01f5
Rhiannon Argote <rhiannon.argote@gmail.com>
2016-02-22 22:22:09 +1100

    Oauth testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,15 +6,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: config.app_domain }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com', 
-    port: '587',
-    enable_starttls_auto: true,
-    user_name: 'rhiannonland@gmail.com',
-    password: ENV["gmail_password"],
-    authentication: :plain,
-    domain: 'airchef.herokuapp.com'
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com', 
+  #   port: '587',
+  #   enable_starttls_auto: true,
+  #   user_name: 'rhiannonland@gmail.com',
+  #   password: ENV["gmail_password"],
+  #   authentication: :plain,
+  #   domain: 'airchef.herokuapp.com'
+  # }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
