74c9668cc9dd68b46dd67099196ec6d3a8d35b57
Rhiannon Argote <rhiannon.argote@gmail.com>
2016-03-28 17:33:17 +1100

    changed config.action_controller.asset_host = 'http://assets.airchefme.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'http://assets.airchefme.com'
 
   # General Settings
   config.app_domain = 'airchefme.com'
